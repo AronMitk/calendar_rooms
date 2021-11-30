aef31344673bbc780cdf6f2fdfd7dd0b1d6d1505
Arnas Mitkevičius <arnasmit@gmail.com>
2021-11-30 17:31:18 +0200

    Initial commit

---
+++ src/main/kotlin/com/harrontech/calendar_rooms/CalendarRoomsApplication.kt
@@ -0,0 +1,11 @@
+package com.harrontech.calendar_rooms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalendarRoomsApplication
+
+fun main(args: Array<String>) {
+	runApplication<CalendarRoomsApplication>(*args)
+}
---
+++ src/test/kotlin/com/harrontech/calendar_rooms/CalendarRoomsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.harrontech.calendar_rooms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CalendarRoomsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
